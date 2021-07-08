543804bf0ae1e3cb3cfd6acb4faf786cb186e0b3
antles <aantles22@gmail.com>
2021-07-07 17:06:20 -0700

    images to links

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,38 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Links to all the fun</h1>
-    <p>
-      <Link to="https://www.twitch.tv/realantles">Twitch</Link> <br />
-      <Link to="https://www.youtube.com/channel/UC2S2m5fhKWZbdf6uNravbTg">YouTube</Link> <br />
-      <Link to="https://www.tiktok.com/@realantles?">TikTok</Link> <br />
-      <Link to="https://twitter.com/RealAntles">Twitter</Link> <br />
-    </p>
+    <h1>Links To All The Fun</h1>
+    <ul>
+      <li>
+        <div>
+          <i class="fab fa-twitch"></i>
+          <Link to="https://www.twitch.tv/realantles">Twitch</Link>
+        </div>
+      </li>
+      <li>
+        <div>
+        <i class="fab fa-youtube"></i>
+          <Link to="https://www.youtube.com/channel/UC2S2m5fhKWZbdf6uNravbTg">YouTube</Link>
+        </div>
+      </li>
+      <li>
+        <div>
+        <i class="fab fa-tiktok"></i>
+          <Link to="https://www.tiktok.com/@realantles?">TikTok</Link> 
+        </div>
+      </li>
+      <li>
+        <div>
+        <i class="fab fa-twitter"></i>
+          <Link to="https://twitter.com/RealAntles">Twitter</Link>
+        </div>
+      </li>
+    </ul>
   </Layout>
 )
 
 export default IndexPage
+
+      
+      
+      
