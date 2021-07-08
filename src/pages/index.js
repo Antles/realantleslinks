1d6e73f7a4d36a38924bd42c2df3d3db2c0f7dad
antles <aantles22@gmail.com>
2021-07-07 19:14:10 -0700

    fix icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -12,25 +13,25 @@ const IndexPage = () => (
     <ul>
       <li>
         <div>
-          <FontAwesomeIcon icon={["fab", "twitch"]} />
+          <FontAwesomeIcon icon="twitch" />
           <Link to="https://www.twitch.tv/realantles">Twitch</Link>
         </div>
       </li>
       <li>
         <div>
-        <FontAwesomeIcon icon={["fab", "youtube"]} />
+        <FontAwesomeIcon icon="youtube" />
           <Link to="https://www.youtube.com/channel/UC2S2m5fhKWZbdf6uNravbTg">YouTube</Link>
         </div>
       </li>
       <li>
         <div>
-        <FontAwesomeIcon icon={["fab", "tiktok"]} />
+        <FontAwesomeIcon icon="tiktok" />
           <Link to="https://www.tiktok.com/@realantles?">TikTok</Link> 
         </div>
       </li>
       <li>
         <div>
-        <FontAwesomeIcon icon={["fab", "twitter"]} />
+        <FontAwesomeIcon icon="twitter" />
           <Link to="https://twitter.com/RealAntles">Twitter</Link>
         </div>
       </li>
