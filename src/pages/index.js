261dfe6d0c0cf698ccbbe52ed6474cee6dfb1767
antles <aantles22@gmail.com>
2021-07-08 10:46:50 -0700

    css changes?

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,25 +15,25 @@ const IndexPage = () => (
       <li>
         <div className='linkContainer'>
           <FontAwesomeIcon icon={faTwitch} className='icon' />
-          <Link to="https://www.twitch.tv/realantles">Twitch</Link>
+          <Link to="https://www.twitch.tv/realantles" className='link'>Twitch</Link>
         </div>
       </li>
       <li>
         <div className='linkContainer'>
           <FontAwesomeIcon icon={faYoutube} className='icon' />
-          <Link to="https://www.youtube.com/channel/UC2S2m5fhKWZbdf6uNravbTg">YouTube</Link>
+          <Link to="https://www.youtube.com/channel/UC2S2m5fhKWZbdf6uNravbTg" className='link'>YouTube</Link>
         </div>
       </li>
       <li>
         <div className='linkContainer'>
           <FontAwesomeIcon icon={faTiktok} className='icon' />
-          <Link to="https://www.tiktok.com/@realantles?">TikTok</Link> 
+          <Link to="https://www.tiktok.com/@realantles?" className='link'>TikTok</Link> 
         </div>
       </li>
       <li>
         <div className='linkContainer'>
           <FontAwesomeIcon icon={faTwitter} className='icon' />
-          <Link to="https://twitter.com/RealAntles">Twitter</Link>
+          <Link to="https://twitter.com/RealAntles" className='link'>Twitter</Link>
         </div>
       </li>
     </ul>
